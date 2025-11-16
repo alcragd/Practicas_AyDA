d38c9ac2470a2500561d2d03cadf4e32a5f9593a
alcra <65140768+alcragd@users.noreply.github.com>
2025-11-15 21:52:38 -0600

    linear search

---
+++ Practica 2/busqueda_lineal.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(int *A,int n,int v);
+
+int main(int argc,char **argv){
+    if(argc!=2)
+        exit(1);
+
+    int n, *A, res, val;
+    n=atoi(argv[1]);
+    A=malloc(n*sizeof(int));
+
+    scanf("%d",val);
+
+    for(int i=0;i<n;++i)
+        scanf("%d",A[i]);
+
+    res=linearSearch(A,n,val);
+
+    printf("\n%d",res);
+
+
+    return 0;
+}
+
+
+int linearSearch(int *A,int n,int v){
+    for(int i=0;i<n;++i)
+        if(A[i]==v)
+            return i;
+    return -1;
+}
+
