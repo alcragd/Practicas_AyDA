b4ef299648fb51d4d48501feabfc83e56f967515
CraftyProgram <migueangelyto@gmail.com>
2025-11-16 15:22:14 -0600

    Update busqueda_binaria.c

--- Practica 2/busqueda_binaria.c
+++ Practica 2/busqueda_binaria.c
@@ -1,29 +1,89 @@
+/*
+Nombre del programa: busqueda_binaria.c
+Version: 1.0 Noviembre 2025
+Autores: Ramos Mendoza Miguel Angel / Coyol Moreno Angel Zoe / Ramirez Hernandez Christian Isaac
+
+Programa que lee n numeros y los coloca en un arreglo A,
+ejecuta un algoritmo que busca un numero v por metodo de
+busqueda binaria
+
+Compilacion: gcc busqueda_binaria.c tiempo/tiempo.c -o busqueda_binaria
+Ejecucion: ./busqueda_binaria n < numeros10millones.txt
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int binarySearch(int *A,int n,int v);
 
-int main(int argc,char **argv){
+int main(int argc,char **argv)
+{
+    // Recibe por argumento el valor de la variable n (tamaño del arreglo a ordenar)
     if(argc!=2)
         exit(1);
 
+    // Variables para el algoritmo
     int n, *A, res, val;
+    
+    double utime0, stime0, wtime0, utime1, stime1, wtime1; // Variables para medición de tiempos
+    // Asigna a n el segundo argumento del main
     n=atoi(argv[1]);
+    
+    // Apartar memoria para n numeros enteros
     A=malloc(n*sizeof(int));
 
-    scanf("%d",&val);
+    if (A == NULL)
+    {
+        printf("\n[!]-- Error al asignar memoria");
+        exit(1);
+    }
 
+    // Lee el valor a buscar en el archivo
+    scanf("%d",&val);
+    // Lee los n valores del archivo 10mil_numeros.txt y los coloca en el arreglo de numeros
+    
     for(int i=0;i<n;++i)
         scanf("%d",&A[i]);
 
+    // Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+    uswtime(&utime0, &stime0, &wtime0);
+
+    // Llama al algoritmo
     res=binarySearch(A,n,val);
 
+    /// Evaluar los tiempos de ejecución
+    uswtime(&utime1, &stime1, &wtime1);
+
+    // Mostrar la posicion del valor en el arreglo
     printf("\n%d",res);
 
+    // Cálculo del tiempo de ejecución del programa
+    printf("\n");
+    printf("real (Tiempo total)  %.10f s\n", wtime1 - wtime0);
+    printf("user (Tiempo de procesamiento en CPU) %.10f s\n", utime1 - utime0);
+    printf("sys (Tiempo en acciónes de E/S)  %.10f s\n", stime1 - stime0);
+    printf("CPU/Wall   %.10f %% \n", 100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+    printf("\n");
+
+    // Mostrar los tiempos en formato exponecial
+    printf("\n");
+    printf("real (Tiempo total)  %.10e s\n", wtime1 - wtime0);
+    printf("user (Tiempo de procesamiento en CPU) %.10e s\n", utime1 - utime0);
+    printf("sys (Tiempo en acciónes de E/S)  %.10e s\n", stime1 - stime0);
+    printf("CPU/Wall   %.10f %% \n", 100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+    printf("\n");
+    //******************************************************************
+    // Libera la memoria correspondiente al arreglo A
     free(A);
-    return 0;
 }
 
+/*
+int binarySearch(int *A,int n,int v)
+Recibe: int * Referencia/Direccion al arreglo A, int tamano del arreglo,
+int valor a buscar en el archivo
+Devuelve: int posicion donde se encuentra el valor en el arreglo
+Observaciones: Funcion que busca un valor en un arreglo por
+metodo de busqueda binaria
+*/
 int binarySearch(int *A,int n,int v){
     int l,r,m;
     l=0; r=n-1;
@@ -35,4 +95,6 @@ int binarySearch(int *A,int n,int v){
         
         A[m] > v ? (r = m - 1) : (l = m + 1);
     }
+    // Retornar -1 si no se encontro el valor
+    return - 1;
 }
