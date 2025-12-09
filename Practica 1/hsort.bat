c893414b2cbf21d9822cc621cc4a802fafaaa6ad
alcra <65140768+alcragd@users.noreply.github.com>
2025-12-09 01:23:08 -0600

    Version funcional pero muy lenta

---
+++ Practica 1/hsort.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Script para ejecutar HSort.exe con numeros10millones.txt
+
+
+HSort.exe < numeros10millones.txt > resultado.txt
+
+REM Muestra el resultado en pantalla
+REM type resultado.txt
+
+REM Pausa para ver la salida
+
+pause
---
+++ Practica 3/clean.bat
@@ -0,0 +1,2 @@
+del uncompressed.*
+del *_compressed.*
